2fd08af2e8ed60db1e38b2002e2f596560d499b8
William.Lieberz <williamlieberz@gmail.com>
2022-10-03 17:29:26 -0700

    Eliminate infinite loop   - The loop may end sooner, but worst-case it will   only ever need to iterate over the length of the input string.

--- src/palindrome/index.js
+++ src/palindrome/index.js
@@ -1,25 +1,24 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
-
 function palindrome(str) {
-    for (i = 1, j = str.length; i != j; i++, j--) {  
+    loopCounter = 0
+    i = 1
+    j = str.length
+
+    while (loopCounter < str.length) {
         let front = i - 1;
-        let back = j - 1;      
+        let back = j - 1;
+
         if (str.charAt(front) != str.charAt(back)) {
             return false
-        }
+        }        
         
-        // Prevent infinite loop and determine when we can stop checking
-        // If j is "one away" from j, we've compared all values:
+        // If i is "one away" from j, we've compared all values:
         if (i + 1 == j) {
             break;
         }
+
+        i++
+        j--
+        loopCounter++
     }
 
     return true
